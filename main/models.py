fc2c9bfc25e4dd21fee6ffea360e1646821a2d08
fuadmmnf <bsse0920@iit.du.ac.bd>
2021-03-10 03:40:30 +0600

    starting api test

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ import json
 import uuid
 
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import datetime #important if using timezones
 
 
 class Company(models.Model):
@@ -18,14 +18,14 @@ class Company(models.Model):
 class DailyData(models.Model):
     unique_id = models.UUIDField(primary_key=True, blank=True, default=uuid.uuid4)
     trading_code = models.CharField(max_length=100, null=False)
-    date = models.DateTimeField(default=timezone.now)
-    last_traded_price = models.DecimalField(decimal_places=3)
-    high = models.DecimalField(decimal_places=3)
-    low = models.DecimalField(decimal_places=3)
-    closing_price = models.DecimalField(decimal_places=3)
-    predicted_next_day_closing_price = models.DecimalField(decimal_places=3)
-    yesterdays_closing_price = models.DecimalField(decimal_places=3)
-    change = models.DecimalField(decimal_places=3)
+    date = models.DateTimeField(default=datetime.now())
+    last_traded_price = models.DecimalField(max_digits=5, decimal_places=3)
+    high = models.DecimalField(max_digits=5, decimal_places=3)
+    low = models.DecimalField(max_digits=5, decimal_places=3)
+    closing_price = models.DecimalField(max_digits=5, decimal_places=3)
+    predicted_next_day_closing_price = models.DecimalField(max_digits=5, decimal_places=3)
+    yesterdays_closing_price = models.DecimalField(max_digits=5, decimal_places=3)
+    change = models.DecimalField(max_digits=5, decimal_places=3)
     trade = models.IntegerField()
-    value_mn = models.DecimalField(decimal_places=3)
-    volume = models.DecimalField(decimal_places=3)
+    value_mn = models.DecimalField(max_digits=5, decimal_places=3)
+    volume = models.DecimalField(max_digits=5, decimal_places=3)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+from django.core import serializers
 from django.http import JsonResponse
-from datetime import datetime
+from django.utils.timezone import datetime #important if using timezones
 from django.views.decorators.csrf import csrf_exempt
 from .models import Company, DailyData
 
@@ -13,8 +14,8 @@ def get_companies(request):
     if request.method == 'POST':
         return JsonResponse({'res': 'must be get request with category_name'})
 
-    companies = Company.objects.get()
-    return JsonResponse({'data': companies}, safe=False)
+    companies = Company.objects.all()
+    return JsonResponse({'data': serializers.serialize('json', companies)}, safe=False)
 
 
 @csrf_exempt
@@ -22,8 +23,9 @@ def get_predictions_by_date(request):
     if request.method == 'POST':
         return JsonResponse({'res': 'must be get request'})
 
-    daily_data = DailyData.objects.filter(date__day=datetime.now().date()).get()
-    return JsonResponse({'data': daily_data}, safe=False)
+
+    daily_data = DailyData.objects.filter(date__day=datetime.now()).get()
+    return JsonResponse({'data': serializers.serialize('json', daily_data)})
 
 
 @csrf_exempt
@@ -32,7 +34,7 @@ def get_data_by_company(request, company_code):
         return JsonResponse({'res': 'must be get request'})
 
     company_predictions = DailyData.objects \
-                              .filter(company_code__exact=company_code) \
+                              .filter(trading_code__exact=company_code) \
                               .order_by('-date')[:30]
 
-    return JsonResponse({'data': company_predictions}, safe=False)
+    return JsonResponse({'data': serializers.serialize('json', company_predictions)}, safe=False)

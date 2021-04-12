49c8ecd4474fd315f8555e4a4281c0ecff9e2e88
fuadmmnf <bsse0920@iit.du.ac.bd>
2021-04-12 12:24:41 +0600

    loading models on demand and added scalers

--- main/apps.py
+++ main/apps.py
@@ -1,20 +1,8 @@
-import glob
-import os
-
-from keras.models import load_model
-import joblib
 
 from django.apps import AppConfig
-from sklearn.preprocessing import MinMaxScaler
 
 
 class MainConfig(AppConfig):
     name = 'main'
-    dse_models = {}
-    for filepath in glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/dse_estimator/models/*.model')[: 20]:
-        print((filepath.split('/')[-1]).split('.')[0])
-        dse_models[(filepath.split('/')[-1]).split('.')[0]] = load_model(filepath)
 
-    minmax_scalers = {}
-    for filepath in glob.glob(os.path.dirname(os.path.abspath(__file__)) + '/dse_estimator/scalers/*.pkl')[: 20]:
-        minmax_scalers[(filepath.split('/')[-1]).split('.')[0]] = joblib.load(open(filepath, 'rb'))
+
--- scrapy_app/dse_scraping/pipelines.py
+++ scrapy_app/dse_scraping/pipelines.py
@@ -1,41 +1,46 @@
 from main.models import DailyData, Company
-from main.apps import MainConfig
 import numpy as np
+import glob
+import os
+
+from keras.models import load_model
+import joblib
+
 
 class DSEScrapingPipeline(object):
     def __process_sharedata(self, item):
         item = {k: v.replace(',', '').replace('--', '0') for k, v in item.items()}
         company_data = DailyData.objects.filter(trading_code__exact=item['trading_code']).order_by('-parsed_date')[: 1]
 
-        if item['trading_code'] in MainConfig.dse_models:
-            if len(company_data) > 0:
-                company_data[0].trading_code = item['trading_code']
-                company_data[0].last_traded_price = round(float(item['last_traded_price']), 3)
-                company_data[0].high = round(float(item['high']), 3)
-                company_data[0].low = round(float(item['low']), 3)
-                company_data[0].closing_price = round(float(item['closing_price']), 3)
-                company_data[0].predicted_next_day_closing_price = round(self.__predict_closing(item), 3)
-                company_data[0].yesterdays_closing_price = round(float(item['yesterdays_closing_price']), 3)
-                company_data[0].change = round(float(item['change']), 3)
-                company_data[0].trade = int(item['trade'])
-                company_data[0].value_mn = round(float(item['value_mn']), 3)
-                company_data[0].volume = round(float(item['volume']), 3)
-                company_data[0].save(force_update=True)
-            else:
-                DailyData.objects.create(
-                    trading_code=item['trading_code'],
-                    last_traded_price=round(float(item['last_traded_price']), 3),
-                    high=round(float(item['high']), 3),
-                    low=round(float(item['low']), 3),
-                    closing_price=round(float(item['closing_price']), 3),
-                    predicted_next_day_closing_price=round(self.__predict_closing(item), 3),
-                    yesterdays_closing_price=round(float(item['yesterdays_closing_price']), 3),
-                    change=round(float(item['change']), 3),
-                    trade=int(item['trade']),
-                    value_mn=round(float(item['value_mn']), 3),
-                    volume=round(float(item['volume']), 3)
+        if len(company_data) > 0:
+            company_data[0].trading_code = item['trading_code']
+            company_data[0].last_traded_price = round(float(item['last_traded_price']), 3)
+            company_data[0].high = round(float(item['high']), 3)
+            company_data[0].low = round(float(item['low']), 3)
+            company_data[0].closing_price = round(float(item['closing_price']), 3)
+            company_data[0].predicted_next_day_ohlc_price = round(self.__predict_ohlc(item).item(), 3)
+            company_data[0].yesterdays_closing_price = round(float(item['yesterdays_closing_price']), 3)
+            company_data[0].change = round(float(item['change']), 3)
+            company_data[0].trade = int(item['trade'])
+            company_data[0].value_mn = round(float(item['value_mn']), 3)
+            company_data[0].volume = round(float(item['volume']), 3)
+            company_data[0].save(force_update=True)
+        else:
+            DailyData.objects.create(
+                trading_code=item['trading_code'],
+                last_traded_price=round(float(item['last_traded_price']), 3),
+                high=round(float(item['high']), 3),
+                low=round(float(item['low']), 3),
+                closing_price=round(float(item['closing_price']), 3),
+                predicted_next_day_ohlc_price=round(self.__predict_ohlc(item).item(), 3),
+                yesterdays_closing_price=round(float(item['yesterdays_closing_price']), 3),
+                change=round(float(item['change']), 3),
+                trade=int(item['trade']),
+                value_mn=round(float(item['value_mn']), 3),
+                volume=round(float(item['volume']), 3)
 
-                )
+            )
+
 
     def __process_companydata(self, item, is_create):
         if is_create:
@@ -54,6 +59,7 @@ class DSEScrapingPipeline(object):
             company.sector = sec
             company.save()
 
+
     def process_item(self, item, spider):
         if spider.name == 'latest_share':
             self.__process_sharedata(item)
@@ -61,13 +67,25 @@ class DSEScrapingPipeline(object):
             self.__process_companydata(item, spider.name == 'companies')
         return item
 
-    def __predict_closing(self, item):
+
+    def __predict_ohlc(self, item):
         ohlc = (float(item['yesterdays_closing_price']) + float(item['high']) + float(item['low']) + float(
             item['closing_price'])) / 4
 
-        scaled_ohlc = MainConfig.minmax_scalers[item['trading_code']].transform([ohlc])
-        predicted_ohlc = MainConfig.dse_models[item['trading_code']].predict(np.reshape(scaled_ohlc, (1, 1, 1))) if item['trading_code'] in MainConfig.dse_models else 0.0
-        predicted_ohlc = MainConfig.minmax_scalers[item['trading_code']].inverse_transform(predicted_ohlc)
-        print('predicted eije')
-        print(item['trading_code'] + ': ' + str(predicted_ohlc))
-        return predicted_ohlc
+        minmax_scaler = self.__load_scaler(item['trading_code'])
+        scaled_ohlc = minmax_scaler.transform([[ohlc]])
+
+        lstm_model = self.__load_model(item['trading_code'])
+        predicted_ohlc = lstm_model.predict(np.reshape(scaled_ohlc, (1, 1, 1))) if lstm_model else 0.0
+        predicted_ohlc = minmax_scaler.inverse_transform(predicted_ohlc)
+
+        return predicted_ohlc[0][0]
+
+
+    def __load_scaler(self, trading_code):
+        filepath = os.path.dirname(os.path.abspath(__file__)) + '/dse_estimator/scalers/' + trading_code + '.pkl'
+        return joblib.load(open(filepath, 'rb'))
+
+    def __load_model(self, trading_code):
+        filepath = os.path.dirname(os.path.abspath(__file__)) + '/dse_estimator/models/' + trading_code + '.model'
+        return load_model(filepath)
